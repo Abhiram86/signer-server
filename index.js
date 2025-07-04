e63564788aadec6ba58f4ab0a7d3a3f2e7407fd3
Abhiram86 <abhiramalla86@gmail.com>
2025-07-04 14:28:00 +0530

    cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: "https://signer-client-gray.vercel.app",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   })
 );
