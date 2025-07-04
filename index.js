d0567c8e6721f00abc21d54ed19d2d2a396ad5a3
Abhiram86 <abhiramalla86@gmail.com>
2025-07-04 14:43:52 +0530

    cors

--- index.js
+++ index.js
@@ -17,27 +17,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://signer-client-gray.vercel.app"
-    );
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Cookie"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.status(204).end();
-  } else {
-    next();
-  }
-});
-
 app.use(express.json());
 app.use(cookieParser());
 
