e7c8df8bc7ace211b96686fcab05b84788db9cdb
Abhiram86 <abhiramalla86@gmail.com>
2025-07-04 21:39:01 +0530

    cors

--- index.js
+++ index.js
@@ -17,20 +17,6 @@ app.use(
   })
 );
 
-app.options("*", (req, res) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://signer-client-gray.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Cookie"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.status(200).end();
-});
-
 app.use(express.json());
 app.use(cookieParser());
 
--- routes/auth.js
+++ routes/auth.js
@@ -10,24 +10,7 @@ import {
 
 const authRouter = express.Router();
 
-authRouter.options("*", (req, res) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://signer-client-gray.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Cookie"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.status(200).end();
-});
-
 authRouter.post("/register", validateRegister, register);
-authRouter.options("/login", (req, res) =>
-  res.status(200).json({ message: "OK" })
-);
 authRouter.post("/login", optionsMiddleware, validateLogin, login);
 authRouter.get("/getuser", validateGetUser, getUser);
 authRouter.post("/logout", validateLogout, logout);
